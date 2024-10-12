ede706834ea793ec16d1f45783af4c7c45fdfa6b
Albert <albertzeap@gmail.com>
2024-10-12 00:29:03 -0700

    Add color customization

--- calendar.js
+++ calendar.js
@@ -6,10 +6,11 @@ let season = document.getElementById("season");
 let celebrationsLi = document.getElementById("celebrations");
 
 const fetchDate = async (dateToday) => {
-    try{
+    try {
+        
         let response = await fetch(`http://calapi.inadiutorium.cz/api/v0/en/calendars/default/${dateToday}`);
         const dateObject = await response.json();
-
+    
         if(!response || dateObject.error){
             let message = `Error: ${response.status}, ${response.statusText}`;
             let err = new Error(message);
@@ -26,19 +27,17 @@ const fetchDate = async (dateToday) => {
     } catch (err){
         console.error(`Error: ${err}`);
     }
-}
+};
 
-// TODO: Implement another api that searches the listed saint celebration upon click
 const getCelebrations = (celebrations) =>{
     celebrations.forEach(celebration => {
         let saint = document.createElement("li");
         saint.innerHTML = `${celebration.rank}: ${celebration.title}`;
         celebrationsLi.append(saint);
     });
-}
+};
 
 const renderDate = () => {
-
     // Obtains the local date of the host
     let currentDate = new Date();
     let cDay = currentDate.getDate();
@@ -48,19 +47,64 @@ const renderDate = () => {
 
     todaysDate.innerHTML = dateToday;
     fetchDate(dateToday);
+};
 
+
+// let primaryColorPicker = document.getElementById("primaryColorPicker");
+let secondaryColorPicker = document.getElementById("secondaryColorPicker");
+let backgroundColorPicker = document.getElementById("backgroundColorPicker");
+let saveColorButton = document.getElementById("saveColor");
+let resetColorButton = document.getElementById("resetColor");
+
+const setEventListeners = () => {
+    // Save the selected color to chrome storage
+    saveColorButton.addEventListener("click", () => {
+        const selectedSecondaryColor = secondaryColorPicker.value ||'#513930';
+        const selectedBackgroundColor = backgroundColorPicker.value || '#AFD5EB';
+        chrome.storage.sync.set({ secondaryColor: selectedSecondaryColor }, () => {
+            todaysDate.style.color = selectedSecondaryColor;
+            weekday.style.color = selectedSecondaryColor;
+            week.style.color = selectedSecondaryColor;
+            season.style.color = selectedSecondaryColor;
+            celebrationsLi.style.color = selectedSecondaryColor;
+        });
+        chrome.storage.sync.set({ backgroundColor: selectedBackgroundColor }, () => {
+            document.body.style.backgroundColor = selectedBackgroundColor;
+            saveColorButton.style.backgroundColor = selectedBackgroundColor;
+        });
+    });   
+    
+    resetColorButton.addEventListener("click", () => {
+        secondaryColorPicker.value = '#513930';
+        backgroundColorPicker.value = '#AFD5EB';
+        chrome.storage.sync.clear();
+    })
 }
 
+// Load and apply the saved background color
+const loadBackgroundColor = () => {
+    try {
+        chrome.storage.sync.get("secondaryColor", (data) => {
+            const color = data.secondaryColor || '#513930'; 
+            todaysDate.style.color = color;
+            weekday.style.color = color;
+            week.style.color = color;
+            season.style.color = color;
+            celebrationsLi.style.color = color;
+            secondaryColorPicker.value = color;
+        });
+        chrome.storage.sync.get("backgroundColor", (data) => {
+            const color = data.backgroundColor || '#AFD5EB'; 
+            document.body.style.backgroundColor = color;
+            saveColorButton.style.backgroundColor = color;
+            backgroundColorPicker.value = color;
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-const colors = ["#AFD5EB", "#D7CBB2"];
-let colorIndex = 0;
-const changeBackground = () => {
-    const nextColor = colors[colorIndex]
-    document.body.style.backgroundColor = nextColor;
 
-    colorIndex = (colorIndex + 1) % colors.length;
-}
-
-
-
+setEventListeners();
+loadBackgroundColor();
 renderDate();
